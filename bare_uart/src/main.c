4e5e0e934d706b3c85d82bebe27b186dde49fffb
Steven Dao <stevendao100@gmail.com>
2025-06-21 15:36:05 -0500

    attempt src

---
+++ bare_uart/src/main.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+
+#define GPIO_OUT_W1TS_REG  0x3FF44008
+#define GPIO_OUT_W1TC_REG  0x3FF4400C
+#define GPIO_ENABLE_REG 0x3FF44020
+
+static volatile uint32_t* gpio_out_w1ts_reg = (uint32_t*)GPIO_OUT_W1TS_REG;
+// static volatile uint32_t* gpio_out_w1tc_reg = (uint32_t*)GPIO_OUT_W1TC_REG;
+static volatile uint32_t* gpio_enable_reg = (uint32_t*)GPIO_ENABLE_REG;
+
+#define GPIO_PIN 2
+
+void this() {
+    // enable GPIO PIN
+    *gpio_enable_reg = (1 << GPIO_PIN);
+
+    // TURN ON
+    *gpio_out_w1ts_reg = (1 << GPIO_PIN);
+
+    // turn off
+    // *gpio_out_w1tc_reg = (1 << GPIO_PIN);
+}
+
+// // Startup logic; this is the application entry point.
+// void __attribute__( ( noreturn ) ) call_start_cpu0() {
+//     this();
+//     while(1) {};
+// }
