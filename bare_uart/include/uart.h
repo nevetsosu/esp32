79ce13c4158e67d367e26773ee9bb12fd1196c5d
Steven Dao <stevendao100@gmail.com>
2025-06-24 18:01:55 -0500

    start converting to one uart register struct, this is currently WORKING

--- bare_uart/include/uart.h
+++ bare_uart/include/uart.h
@@ -1,9 +1,70 @@
-// #define UART_FIFO_REG         (volatile uint32_t*)0x3FF40000
-// #define UART_STATUS_REG       (volatile uint32_t*)0x3FF4001C
-// #define UART_CLKDIV_REG       (volatile uint32_t*)0x3FF40014
-// #define UART_CONF0_REG        (volatile uint32_t*)0x3FF40020
 #pragma once
+#include <stdint.h>
+
+#define UART0_BASE          0x3FF40000
+#define UART1_BASE          0x3FF50000
+#define UART2_BASE          0x3FF6E000
+
+typedef struct uart_regs_t {
+    volatile uint32_t FIFO;
+    volatile uint32_t INT_RAW;
+    volatile uint32_t INT_ST;
+    volatile uint32_t INT_ENA;
+    volatile uint32_t INT_CLR;
+    volatile uint32_t CLKDIV;
+    volatile uint32_t AUTOBAUD;
+    volatile uint32_t STATUS;
+    volatile uint32_t CONF0;
+    volatile uint32_t CONF1;
+    volatile uint32_t LOWPULSE;
+    volatile uint32_t HIGHPULSE;
+    volatile uint32_t RXD_CNT;
+    volatile uint32_t FLOW_CONF;
+    volatile uint32_t SLEEP_CONF;
+    volatile uint32_t SWFC_CONF;
+    volatile uint32_t IDLE_CONF;
+    volatile uint32_t RS485_CONF;
+    volatile uint32_t AT_CMD_PRECNT;
+    volatile uint32_t AT_CMD_POSTCNT;
+    volatile uint32_t AT_CMD_GAPTOUT;
+    volatile uint32_t AT_CMD_CHAR;
+    volatile uint32_t MEM_CONF;
+    volatile uint32_t MEM_TX_STATUS;
+    volatile uint32_t MEM_RX_STATUS;
+    volatile uint32_t MEM_CNT_STATUS;
+    volatile uint32_t POSPULSE;
+    volatile uint32_t NEGPULSE;
+} uart_regs_t;
+
+#define UART0       ((uart_regs_t*)(0x3FF40000))
+#define UART1       ((uart_regs_t*)(0x3FF50000))
+#define UART2       ((uart_regs_t*)(0x3FF6E000))
 
 void uart0_init();
 void uart0_write_str(const char* s);
 void uart0_write_byte(char c);
+void uart0_write_str_n(const char* s, uint32_t n);
+
+inline uint32_t UART_CLKDIV_INT(uint32_t clk, uint32_t baudrate) {
+    return (clk / baudrate) & 0x000FFFFF;
+}
+
+inline uint32_t UART_CLKDIV_FRAG(uint32_t clk, uint32_t baudrate) {
+    return ((((clk % baudrate) * 16) / baudrate) & 0xF) << 20;
+}
+
+inline uint32_t UART_GET_TX_SIZE(uart_regs_t* uart) {
+    return ((uart->MEM_CONF >> 7) & 0xF);
+}
+
+inline uint32_t UART_GET_RX_SIZE(uart_regs_t* uart) {
+    return ((uart->MEM_CONF >> 3) & 0xF);
+}
+
+inline uint32_t UART_GET_RX_COUNT(uart_regs_t* uart) {
+    return (uart->STATUS & 0xFF);
+}
+
+inline uint32_t UART_GET_TX_COUNT(uart_regs_t* uart) {
+    return ((uart->STATUS >> 16) & 0xFF);
+}
--- bare_uart/src/uart.c
+++ bare_uart/src/uart.c
@@ -1,35 +1,10 @@
-#include "regs.h"
 #include "uart.h"
+#include "regs.h"
 #include "gpio.h"
 
 #define APB_CLK_FREQ        80000000  // APB clock = 80 MHz
 
-#define UART0_BASE          0x3FF40000
-#define UART0_FIFO_REG       (volatile uint32_t*)(UART0_BASE + 0x0)
-// #define UART_INT_RAW_REG  *(volatile uint32_t*)(UART0_BASE + 0x4)
-// #define UART_INT_ST_REG     *(volatile uint32_t*)(UART0_BASE + 0x8)
-#define UART0_INT_ENA_REG    (volatile uint32_t*)(UART0_BASE + 0xC)
-#define UART0_INT_CLR_REG    (volatile uint32_t*)(UART0_BASE + 0x10)
-#define UART0_CLKDIV_REG     (volatile uint32_t*)(UART0_BASE + 0x14)
-#define UART0_AUTOBAUD_REG   (volatile uint32_t*)(UART0_BASE + 0x18)
-#define UART0_STATUS_REG     (volatile uint32_t*)(UART0_BASE + 0x1C)
-#define UART0_CONF0_REG      (volatile uint32_t*)(UART0_BASE + 0x20)
-#define UART0_CONF1_REG      (volatile uint32_t*)(UART0_BASE + 0x24)
-#define UART0_LOWPULSE_REG   (volatile uint32_t*)(UART0_BASE + 0x28)
-#define UART0_HIGHPULSE_REG  (volatile uint32_t*)(UART0_BASE + 0x2c)
-#define UART0_RXD_CNT_REG    (volatile uint32_t*)(UART0_BASE + 0x30)
-#define UART0_FLOW_CONF_REG  (volatile uint32_t*)(UART0_BASE + 0x34)
-#define UART0_SLEEP_CONF_REG (volatile uint32_t*)(UART0_BASE + 0x38)
-#define UART0_SWFC_CONF_REG  (volatile uint32_t*)(UART0_BASE + 0x3c)
-#define UART0_IDLE_CONF_REG  (volatile uint32_t*)(UART0_BASE + 0x40)
-
-#define UART0_BAUD 115200
-#define UART0_CLKDIV_INT(clk, baudrate)  (((clk) / baudrate) & 0x000FFFFF)
-#define UART0_CLKDIV_FRAG(clk, baudrate) (((((clk % baudrate) * 16) / baudrate) & 0xF) << 20)
-#define UART_GET_TX_SIZE(uart_mem_conf_reg) ((*uart_mem_conf_reg >> 7) & 0xF)
-#define UART_GET_RX_SIZE(uart_mem_conf_reg) ((*uart_mem_conf_reg >> 3) & 0xF)
-#define UART_GET_RX_COUNT(uart_status_reg)  (*uart_status_reg & 0xFF)
-#define UART_GET_TX_COUNT(uart_status_reg)  ((*uart_status_reg >> 16) & 0xFF)
+#define UART0_BAUD 115200 // default baud
 
 static inline void conf_uart0_gpio() {
     *IO_MUX_U0RXD_REG &= ~(0x7 << 12);  // Connect UART to U0RXD pin
@@ -48,33 +23,48 @@ inline void uart0_init() {
     conf_uart0_gpio();
 
     // Disable interrupts for now
-    *UART0_INT_ENA_REG = 0;
+    UART0->INT_ENA = 0;
 
     // Disable AUTOBAUD
-    REG_BIT_CLEAR(UART0_AUTOBAUD_REG, 0);
+    UART0->AUTOBAUD &= ~1;
+    // Set CLKDIV
+
+    // TODO Fix the CLKDIV calculation
+    // UART0->CLKDIV = UART_CLKDIV_INT(APB_CLK_FREQ, UART0_BAUD) | UART_CLKDIV_FRAG(APB_CLK_FREQ, UART0_BAUD);
 
     // Configure UART0 CONF REG
-    *UART0_CONF0_REG = 0;
-    REG_BIT_SET(UART0_CONF0_REG, 27);        // Select APB Clock (80Mhz)
-    REG_BIT_SET(UART0_CONF0_REG, 4);         // Set length of stop bit to 1
-    *UART0_CONF0_REG |= (3 << 2);            // Set length of message (3 = 8bits), starting at bit 2
-    REG_BIT_CLEAR(UART0_CONF0_REG, 23);      // Disable Receive-timeout function
+    if (&UART0->CONF0 != (void*)(UART0_BASE + 0x20)) {
+        while(1) {}
+    }
 
-    // Set CLKDIV
-    // *UART0_CLKDIV_REG = UART0_CLKDIV_INT(APB_CLK_FREQ, UART0_BAUD) | UART0_CLKDIV_FRAG(APB_CLK_FREQ, UART0_BAUD);
+    uint32_t conf0 = 0;
+    conf0 |= (1 << 27);         // Select APB Clock (80Mhz)
+    conf0 |= (1 << 4);          // Set length of stop bit to 1
+    conf0 |= (3 << 2);          // Set length of message (3 = 8bits), starting at bit 2
+    conf0 &= ~(1 << 23);        // Disable Receive-timeout function
+    UART0->CONF0 = conf0;
 
-    REG_BIT_SET(UART0_CONF0_REG, 18);        // Reset the UART transmit-FIFO
-    REG_BIT_SET(UART0_CONF0_REG, 17);        // Reset the UART receive-FIFO
+    // Reset the UART transmit-FIFO and receive-FIFO
+    conf0 |= (3 << 17);
+    UART0->CONF0 = conf0;
+    // clear the reset bits on another write
+    conf0 &= ~(3 << 17);
+    UART0->CONF0 = conf0;
 }
 
 extern volatile uint32_t* gpio_out_w1ts_reg;
 
 inline void uart0_write_byte(char c) {
     // wait until a byte is available
-    while ((128u - UART_GET_TX_COUNT(UART0_STATUS_REG) < 2) | (*UART0_STATUS_REG & (0xFF << 16))) {};
-    *UART0_FIFO_REG = (uint8_t)c;
+    // (128u - UART_GET_TX_COUNT(UART0) < 2) |
+    while ((UART0->STATUS & (0xFF << 16))) {};
+    UART0->FIFO = (uint8_t)c;
 }
 
 inline void uart0_write_str(const char* s) {
     while (*s) uart0_write_byte(*s++);
+}
+
+inline void uart0_write_str_n(const char* s, uint32_t n) {
+    while (n--) uart0_write_byte(*s++);
 }
